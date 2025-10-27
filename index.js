bacc05c1ccb86d2b6834e5c81db069477890d11d
itsukichan <kou05ituki09design@gmail.com>
2025-10-27 14:10:22 +0900

    pwa and service worker

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 async function fetchData() {
   const response = await fetch("https://jsonplaceholder.typicode.com/posts/");
   const data = await response.json();
-
   return data;
 }
 
 const data = fetchData().then((data) => {
-  console.log(data);
   const app = document.getElementById("app");
-
   app.innerHTML = `
-  ${data.map((post) => `<p>${post.title}</p>`).join("")}
+  ${data
+    .map(
+      (post) => `
+      <div class="post">
+        <p>${post.id} : ${post.title}</p>
+      </div>`
+    )
+    .join("")}
   `;
 });
+
+// Register Service Worker
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/sw.js")
+      .then((registration) => {
+        console.log("SW registered: ", registration);
+      })
+      .catch((registrationError) => {
+        console.log("SW registration failed: ", registrationError);
+      });
+  });
+}
